f929b54fec3f95928a2bfac40b9400b1ebbca04d
Noel Delgado <auhcsei@gmail.com>
2013-04-24 03:17:11 -0500

    init files

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 Values.js
 =========
 
-JS library to get the tints and shades of a color
+The lightness or darkness of a color is called its value.
+
+Tints are light values that are made by mixing a color with white, which increases lightness.
+
+Shades are dark values that are made by mixing a color with black, which reduces lightness.
+
+### Usage
+
+```js
+var myColor = new Values();
+myColor.setColor('#0099ff');
+
+var tints = myColor.getTints();
+var shades = myColor.getShades();
+
+var allValues = myColor.getTintsAndShades();
+```
+
+### Defaults
+
+```js
+color: {
+    hex: "#37D7C2",
+    rgb: {r: 55,  g: 215, b: 194},
+    hsl: {h: 172, s: 67,  l: 53}
+},
+gap: 1.1
+```
---
+++ values.js
@@ -0,0 +1,187 @@
+var Values = function ( options ) {
+
+    "use strict";
+
+    this.settings = {
+        color: {
+            hex: "#37D7C2",
+            rgb: {r: 55,  g: 215, b: 194},
+            hsl: {h: 172, s: 67,  l: 53}
+        },
+        gap: 1.1
+    };
+
+    for ( var i in options ) {
+        if ( options.hasOwnProperty(i) ) {
+            this.settings[i] = options[i];
+        }
+    }
+
+    var HEXtoRGB = function ( color ) {
+        color = color.replace('#', '');
+        var r, g, b;
+        if ( color.length === 6 ) {
+            r = parseInt(color.slice(0,2), 16),
+            g = parseInt(color.slice(2,4), 16),
+            b = parseInt(color.slice(4,6), 16);
+        } else {
+            r = parseInt(color.slice(0,1) + color.slice(0,1), 16),
+            g = parseInt(color.slice(1,2) + color.slice(1,2), 16),
+            b = parseInt(color.slice(2,3) + color.slice(2,3), 16);
+        }
+        return {
+            r : r,
+            g : g,
+            b : b
+        };
+    };
+
+    var RGBtoHEX = function (r, g, b) {
+        var r = parseInt(r).toString(16),
+            g = parseInt(g).toString(16),
+            b = parseInt(b).toString(16);
+        r = r.length === 1 ? "0" + r : r;
+        g = g.length === 1 ? "0" + g : g;
+        b = b.length === 1 ? "0" + b : b;
+        return '#' + r + '' + g + '' + b;
+    };
+
+    var RGBtoHSL = function( r, g, b ) {
+        var r   = r/255, g   = g/255, b   = b/255,
+            max = Math.max(r, g, b), min = Math.min(r, g, b),
+            h, s, l = (max + min) / 2;
+        if (max === min) {
+            h = s = 0; // achromatic
+        } else {
+            var d = max - min;
+            s = l > 0.5
+                ? d / (2 - max - min)
+                : d / (max + min);
+            switch( max ) {
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+            h /= 6;
+        }
+        return {
+            h: h,
+            s: s,
+            l: l
+        };
+    };
+
+    var HUEtoRGB = function (v1, v2, vh) {
+        if ( vh < 0 ) vh += 1;
+        if ( vh > 1 ) vh -= 1;
+        if ( (6 * vh) < 1 ) return ( v1 + (v2 - v1) * 6 * vh );
+        if ( (2 * vh) < 1 ) return v2;
+        if ( (3 * vh) < 2 ) return ( v1 + (v2 - v1) * ((2/3) - vh) * 6 );
+        return v1;
+    }
+
+     var HSLtoRGB = function(h, s, l) {
+        var r, g, b;
+
+        if(s == 0){
+            r = g = b = l; // achromatic
+        } else {
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = HUEtoRGB(p, q, h + 1/3);
+            g = HUEtoRGB(p, q, h);
+            b = HUEtoRGB(p, q, h - 1/3);
+        }
+
+        return {
+            r: r * 255,
+            g: g * 255,
+            b: b * 255
+        };
+    };
+
+    Values.prototype.getTints = function() {
+        var h = this.settings.color.hsl.h,
+            s = this.settings.color.hsl.s,
+            l = this.settings.color.hsl.l,
+            i = 100,
+            tints = [];
+
+        while ( i > parseInt(l, 10) ) {
+            var rgb = HSLtoRGB( h / 360, s / 100, i / 100),
+                obj = {};
+            obj.rgb = {r: Math.floor(rgb.r), g: Math.floor(rgb.g), b: Math.floor(rgb.b) };
+            obj.hsl = {h: h, s: s, l: i};
+            obj.hex = RGBtoHEX( obj.rgb.r, obj.rgb.g, obj.rgb.b );
+            tints.push( obj );
+            i -= this.settings.gap;
+        };
+
+        return tints;
+    };
+
+    Values.prototype.getShades = function() {
+        var h = this.settings.color.hsl.h,
+            s = this.settings.color.hsl.s,
+            l = this.settings.color.hsl.l,
+            i = 0,
+            shades = [];
+
+        while ( i < parseInt(l, 10) ) {
+            var rgb = HSLtoRGB( h / 360, s / 100, i / 100),
+                obj = {};
+            obj.rgb = {r: Math.floor(rgb.r), g: Math.floor(rgb.g), b: Math.floor(rgb.b) };
+            obj.hsl = {h: h, s: s, l: i};
+            obj.hex = RGBtoHEX( obj.rgb.r, obj.rgb.g, obj.rgb.b );
+            shades.push( obj );
+            i += this.settings.gap;
+        };
+
+        return shades.reverse();
+    };
+
+    Values.prototype.getTintsAndShades = function() {
+        var tints   = this.getTints(),
+            shades  = this.getShades(),
+            color   = this.settings.color,
+            all     = [];
+
+        for ( var i in tints ) {
+            if ( tints.hasOwnProperty(i) ) {
+                all.push( tints[i] );
+            }
+        }
+
+        all.push( color );
+
+        for ( var i in shades ) {
+            if ( shades.hasOwnProperty(i) ) {
+                all.push( shades[i] );
+            }
+        }
+
+        return all;
+    };
+
+    Values.prototype.getColor = function() {
+        return this.settings.color;
+    };
+
+    Values.prototype.getGap = function() {
+        return this.settings.gap;
+    };
+
+    Values.prototype.setGap = function( value ) {
+        this.settings.gap = value;
+    };
+
+    Values.prototype.setColor = function( value ) {
+        var rgb = HEXtoRGB( value ),
+            hsl = RGBtoHSL( rgb.r, rgb.g, rgb.b );
+        this.settings.color = {
+            hex: value,
+            rgb: rgb,
+            hsl: { h: hsl.h * 360, s: hsl.s * 100, l: hsl.l * 100 }
+        };
+    };
+}
