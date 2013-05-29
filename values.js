5932ccb7c042f668af3d6bf4f371cb6b2aea8ec3
Noel Delgado <auhcsei@gmail.com>
2013-05-29 02:48:53 -0500

    adding hsl and rgb text as attr

--- values.js
+++ values.js
@@ -9,7 +9,9 @@
             color: {
                 hex: "#37D7C2",
                 rgb: {r: 55,  g: 215, b: 194},
-                hsl: {h: 172, s: 67,  l: 53}
+                hsl: {h: 172, s: 67,  l: 53},
+                hslText: 'hsl(172, 67%, 53%)',
+                rgbText: 'rgb(55, 215, 194)'
             },
             range: 1
         };
@@ -123,6 +125,8 @@
             obj.rgb = {r: Math.round(rgb.r), g: Math.round(rgb.g), b: Math.round(rgb.b) };
             obj.hsl = {h: h, s: s, l: i};
             obj.hex = RGBtoHEX( obj.rgb.r, obj.rgb.g, obj.rgb.b );
+            obj.hslText = 'hsl(' + h + ', ' + s + '%, ' + i + '%)';
+            obj.rgbText = 'rgb(' + obj.rgb.r + ', ' + obj.rgb.g + ', ' + obj.rgb.b + ')';
             tints.push( obj );
             i -= this.settings.range;
         };
@@ -147,6 +151,8 @@
             obj.rgb = {r: Math.round(rgb.r), g: Math.round(rgb.g), b: Math.round(rgb.b) };
             obj.hsl = {h: h, s: s, l: i};
             obj.hex = RGBtoHEX( obj.rgb.r, obj.rgb.g, obj.rgb.b );
+            obj.hslText = 'hsl(' + h + ', ' + s + '%, ' + i + '%)';
+            obj.rgbText = 'rgb(' + obj.rgb.r + ', ' + obj.rgb.g + ', ' + obj.rgb.b + ')';
             shades.push( obj );
             i += this.settings.range;
         };
@@ -203,7 +209,9 @@
         this.settings.color = {
             hex: value,
             rgb: rgb,
-            hsl: {h: h, s: s, l: l}
+            hsl: {h: h, s: s, l: l},
+            hslText: 'hsl(' + h + ', ' + s + '%, ' + l + '%)',
+            rgbText: 'rgb(' + rgb.r + ', ' + rgb.g + ', ' + rgb.b + ')'
         };
         return this;
     };
@@ -237,6 +245,8 @@
             };
 
             obj.hex = RGBtoHEX( obj.rgb.r, obj.rgb.g, obj.rgb.b );
+            obj.hslText = 'hsl(' + h + ', ' + s + '%, ' + lightness + '%)';
+            obj.rgbText ='rgb(' + rgb.r + ', ' + rgb.g + ', ' + rgb.b + ')';
 
             return include_base_color ? [this.getColor(), obj] : [obj];
         }
