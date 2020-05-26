509fb4f716f7d44b62d00f741bc0997bee3675d2
Noel Delgado <auhcsei@gmail.com>
2020-05-26 00:24:28 -0600

    add rollup-plugin-sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import buble from '@rollup/plugin-buble';
 import replace from '@rollup/plugin-replace';
 import cleanup from 'rollup-plugin-cleanup';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
@@ -32,9 +33,15 @@ export default [
       'pure-color/convert/hsl2rgb',
       'pure-color/convert/rgb2hex'
     ],
-    plugins: [resolve(), commonjs(), cleanup(), replace({ __VERSION__: `v${pkg.version}` })],
+    plugins: [
+      sourcemaps(),
+      resolve(),
+      commonjs(),
+      cleanup(),
+      replace({ __VERSION__: `v${pkg.version}` })
+    ],
     output: [
-      { file: pkg.main, format: 'cjs' },
+      { file: pkg.main, format: 'cjs', sourcemap: true },
       { file: pkg.module, format: 'es' }
     ]
   }
