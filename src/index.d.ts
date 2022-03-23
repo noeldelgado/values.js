0b2a7f1720b967021443540684297d7ee1fbcecc
Noel Delgado <auhcsei@gmail.com>
2022-03-22 22:00:55 -0700

    🏷️ add types declaration file

---
+++ src/index.d.ts
@@ -0,0 +1,22 @@
+declare module "values.js" {
+
+  export = Values
+
+  class Values {
+    constructor(color?: string, type?: 'base' | 'tint' | 'shade', weight?: number)
+    alpha: number
+    rgb: string
+    type: string
+    weight: number
+    hex: string
+    setColor(color: string): Values
+    tint(weight?: number): Values
+    tints(weight?: number): Values[]
+    shade(weight?: number): Values
+    shades(weight?: number): Values[]
+    all(weight?: number): Values[]
+    hexString(): string
+    rgbString(): string
+    getBrightness(): number
+  }
+}
